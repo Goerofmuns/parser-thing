f009886242f82def5f96d90bbd8d855c3f15f42a
Tom Hutchings <goerofmuns@gmail.com>
2016-07-13 09:19:24 -0700

    juggling syntax

--- src/parser_thing/core.clj
+++ src/parser_thing/core.clj
@@ -4,19 +4,21 @@
 (def grammar
  (insta/parser (slurp "./spec.ebnf")))
 
-(defn extract-val [container]
-  "Extracts the value from a syntax container"
-  (Integer. (last container)))
-
 (defn extract-opr [container]
   (case (last container)
     "+" '+
     "-" '-
     "*" '*))
 
+(defn extract-val [container]
+  "Extracts the value from a syntax container"
+  (case (first container)
+    :sym_Number (Integer. (last container))
+    :sym_Operator (extract-opr container)))
+
 (defn translate [tree]
  "translates a syntax tree into a clojure program"
-  (list (extract-opr (nth tree 1)) (extract-val (first tree)) (extract-val (last tree))))
+  (list (extract-val (nth tree 1)) (extract-val (first tree)) (extract-val (last tree))))
 
 (defn parse [s]
  (grammar s))
