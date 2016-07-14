1d7fc58614a0cf61afe85fa1630a84f063b0feb5
Tom Hutchings <goerofmuns@gmail.com>
2016-07-13 21:23:32 -0700

    christ this is getting weird

--- src/parser_thing/core.clj
+++ src/parser_thing/core.clj
@@ -17,10 +17,16 @@
     :sym_Operator (extract-opr container)))
 
 (defn translate [tree]
- "translates a syntax tree into a clojure program"
-  (list (extract-val (nth tree 1))  ; second element, the operator
-        (extract-val (first tree))  ; first num
-        (extract-val (last tree)))) ; second num
+ "translates a syntax tree into a clojure program
+  First it walks down through the tree, displatching each
+  type of element to its corresponding function"
+  (defn trans-exp_Arithmetic [tree]
+   (list (extract-val (nth tree 1))   ; second element, the operator
+         (extract-val (first tree))   ; first num
+         (extract-val (last tree)))) ; second num
+  (case (first (first container))
+    :exp_Arithmetic (trans-exp_Arithmetic tree)
+    :exp_Function (println "FUNCTION thiNG AAGAG")))
 
 (defn exec [s]
  (eval (translate (grammar s))))
