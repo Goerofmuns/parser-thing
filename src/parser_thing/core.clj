e77ff224b2e64c362133efda3c4442dd375d5666
Tom Hutchings <goerofmuns@gmail.com>
2016-07-13 21:28:02 -0700

    functions are parsed

--- src/parser_thing/core.clj
+++ src/parser_thing/core.clj
@@ -2,7 +2,7 @@
  (:require [instaparse.core :as insta]))
 
 (def grammar
-   (insta/parser (slurp "./spec.ebnf")))
+ (insta/parser (slurp "./spec.ebnf")))
 
 (defn extract-opr [container]
   (case (last container)
@@ -20,11 +20,11 @@
  "translates a syntax tree into a clojure program
   First it walks down through the tree, displatching each
   type of element to its corresponding function"
-  (defn trans-exp_Arithmetic [tree]
-   (list (extract-val (nth tree 1))   ; second element, the operator
-         (extract-val (first tree))   ; first num
-         (extract-val (last tree)))) ; second num
-  (case (first (first container))
+  (defn trans-exp_Arithmetic [_tree]
+   (list (extract-val (nth _tree 1))   ; second element, the operator
+         (extract-val (first _tree))   ; first num
+         (extract-val (last _tree)))) ; second num
+  (case (first (first tree))
     :exp_Arithmetic (trans-exp_Arithmetic tree)
     :exp_Function (println "FUNCTION thiNG AAGAG")))
 
